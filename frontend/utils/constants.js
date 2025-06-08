834096b32505e268e2aeb0cc642a049e4aa94cf3
Selma <selma.karasoftic@stu.ibu.edu.ba>
2025-06-09 00:32:24 +0200

    milestone 5

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -2,5 +2,5 @@ let Constants = {
     PROJECT_BASE_URL: "http://localhost/SelmaKarasoftic/WebProgramming/backend/",
     USER_ROLE: "user",
     ADMIN_ROLE: "admin"
- }
- export default Constants;
+}
+export default Constants;
